32154a34da0cf6c736ce7602eca04f609531129d
hoangnhancs <thaihoangnhantk17lqd@gmail.com>
2026-04-25 09:19:26 +0700

    update

---
+++ server/IdentityService/Migrations/20260425020146_UpdateIdentityService20260425_090142.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIdentityService20260425_090142 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ server/ProductService/Migrations/20260425020120_UpdateProductService20260425_090112.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductService20260425_090112 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
