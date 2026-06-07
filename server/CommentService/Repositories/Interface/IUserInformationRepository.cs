0f8a989765c61d1af80f2bc7c4a593bb8706979c
hoangnhancs <thaihoangnhantk17lqd@gmail.com>
2026-06-07 20:35:32 +0700

    add userinfo repo

---
+++ server/CommentService/Repositories/Interface/IUserInformationRepository.cs
@@ -0,0 +1,10 @@
+﻿using CommentService.Entities;
+using Shared.Core.EF.Domain.Repositories;
+
+namespace CommentService.Repositories.Interface
+{
+    public interface IUserInformationRepository : IBaseEFRepository<UserInformation, int>
+    {
+
+    }
+}
---
+++ server/CommentService/Repositories/UserInformationRepository.cs
@@ -0,0 +1,14 @@
+﻿using CommentService.Data;
+using CommentService.Entities;
+using CommentService.Repositories.Interface;
+using Infrastructure.EF.Repositories;
+
+namespace CommentService.Repositories
+{
+    public class UserInformationRepository : BaseEFRepository<UserInformation, int, CommentSvcDbContext>, IUserInformationRepository
+    {
+        public UserInformationRepository(CommentSvcDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
