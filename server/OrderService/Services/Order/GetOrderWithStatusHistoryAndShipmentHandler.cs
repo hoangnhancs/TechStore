25966e702c935d2c6e50c1be9f0ca53a576b496e
nhan.th1 <nhan.th1@odh.com.vn>
2026-06-22 16:38:00 +0700

    update GetOrderWithStatusHistoryAndShipmentHandler

--- server/OrderService/Services/Order/GetOrderWithStatusHistoryAndShipmentHandler.cs
+++ server/OrderService/Services/Order/GetOrderWithStatusHistoryAndShipmentHandler.cs
@@ -1,13 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using OrderService.DTOs;
 using OrderService.Persistence;
 using Shared.Core.EF.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace OrderService.Services.Order
 {
@@ -15,10 +16,13 @@ namespace OrderService.Services.Order
     {
         private readonly IOrderUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        public GetOrderWithStatusHistoryAndShipmentHandler(IOrderUnitOfWork unitOfWork, IMapper mapper)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public GetOrderWithStatusHistoryAndShipmentHandler(IOrderUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<AppResult<OrderDto>> Handle(GetOrderWithStatusHistoryAndShipmentQuery request, CancellationToken cancellationToken)
         {
@@ -26,11 +30,15 @@ namespace OrderService.Services.Order
                 p => p.Id == request.OrderId,
                 q => q.Include(o => o.StatusHistories).Include(o => o.Shipment).Include(o => o.Items)
             )).FirstOrDefault();
+            var roles = _httpContextAccessor.HttpContext?.User
+                .FindAll(ClaimTypes.Role)
+                .Select(x => x.Value.ToLower())
+                .ToList();
             if (order == null)
             {
                 return AppResult<OrderDto>.Failure("Order not found", 404);
             }
-            if (order.UserId != request.UserId)
+            if (order.UserId != request.UserId && (roles != null && !roles.Contains("admin")))
             {
                 return AppResult<OrderDto>.Failure("Unauthorized access to order history", 403);
             }
